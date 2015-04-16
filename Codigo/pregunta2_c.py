b77891bbe04cba0056afd5328b05fc560ce846d5
Oscar Rencoret <o.rencoret@gmail.com>
2015-04-16 17:29:35 -0400

    Fix error pregunta2_c

--- Codigo/pregunta2_c.py
+++ Codigo/pregunta2_c.py
@@ -5,4 +5,4 @@ while(((x_2 + e_mach/2) - x_2) != 0):
 	x_1 = x_2
 	x_2 += e_mach/2
 	print(x_2)
-print("El menor numero en el cual la suma no es representables es: " + str(x_1))
+print("El mayor numero en el cual la suma es representables es: " + str(x_1))
